6ba8428a4f0e336b591ac0364efb7889d7f002c6
Brook Elgie <specialbrew@gmail.com>
2010-06-16 09:49:43 +0100

    New panel style.

--- imgtag.safariextension/injected.js
+++ imgtag.safariextension/injected.js
@@ -5,15 +5,47 @@ document.oncontextmenu = function(event) {
   safari.self.tab.setContextMenuEventUserInfo(event, isCurrentlyOverImg);
 };
 
-function _removePanel(event) {
-  if (event.target.className != "lcd_panel_element") {
-    if (document.getElementById('lcd_imgtag_panel')) {
-      var panel = document.getElementById('lcd_imgtag_panel');
-      var panel_parent = panel.parentNode;
-      panel_parent.removeChild(panel);
-      document.removeEventListener('click', _removePanel);
-    }
+function _onCopyCut(event) {
+	setTimeout(function(){
+	  _removePanel();
+	}, 0);
+}
+function _onClick(event) {
+  if (event.target && event.target.className != "lcd_panel_element") {
+    _removePanel();
+  } else {
+    document.getElementById('lcd_imgtag_textarea').focus();
+    document.getElementById('lcd_imgtag_textarea').select();
   }
+}
+function _onKeyDown(event) {
+  // If the escape key was pressed.
+  if(event.keyCode == 27) {
+    _removePanel();
+  }
+}
+
+function _removePanel() {
+  if (document.getElementById('lcd_imgtag_panel')) {
+    var panel = document.getElementById('lcd_imgtag_panel');
+    var panel_parent = panel.parentNode;
+    removeListeners();
+    panel_parent.removeChild(panel);
+  } 
+}
+
+function addListeners() {
+  document.addEventListener('click', _onClick);
+  document.addEventListener('copy', _onCopyCut);
+  document.addEventListener('cut', _onCopyCut);
+  document.addEventListener('keydown', _onKeyDown);
+}
+
+function removeListeners() {
+  document.removeEventListener('click', _onClick);
+  document.removeEventListener('copy', _onCopyCut);
+  document.removeEventListener('cut', _onCopyCut);
+  document.removeEventListener('keydown', _onKeyDown);
 }
 
 function createImgTag() {
@@ -33,24 +65,28 @@ function createImgTag() {
 
 function addTextAreaPanel() {
   if (window !== window.top) return;
-  var objPanel, objTextArea, objContainer;
+  var panel, textArea, cloverleaf_c;
 
   if (!document.getElementById('lcd_imgtag_textarea')) {
-    objTextArea = document.createElement('textarea');
-    objTextArea.setAttribute('id','lcd_imgtag_textarea');
-    objTextArea.setAttribute('class', 'lcd_panel_element');
+    textArea = document.createElement('textarea');
+    textArea.setAttribute('id','lcd_imgtag_textarea');
+    textArea.setAttribute('class', 'lcd_panel_element');
 
-    objContainer = document.createElement('div');
-    objContainer.appendChild(objTextArea);
-    objContainer.setAttribute('class', 'lcd_panel_element');
+    cloverleaf_c = document.createElement('img');
+    cloverleaf_c.src = safari.extension.baseURI + "cloverleaf_c.png";
+    cloverleaf_c.setAttribute('height', '38');
+    cloverleaf_c.setAttribute('width', '113');
+    cloverleaf_c.setAttribute('alt', '⌘+C');
+    cloverleaf_c.setAttribute('class', 'lcd_panel_element');
 
-    objPanel = document.createElement('div');
-    objPanel.setAttribute('id','lcd_imgtag_panel');
-    objPanel.setAttribute('class', 'lcd_panel_element');
-    objPanel.appendChild(objContainer);
+    panel = document.createElement('div');
+    panel.setAttribute('id','lcd_imgtag_panel');
+    panel.setAttribute('class', 'lcd_panel_element');
+    panel.appendChild(cloverleaf_c);
+    panel.appendChild(textArea);
 
-    document.body.appendChild(objPanel);
-    document.addEventListener('click', _removePanel);
+    document.body.appendChild(panel);
+    addListeners();
   } 
   document.getElementById('lcd_imgtag_textarea').innerText = createImgTag();
   document.getElementById('lcd_imgtag_textarea').select();
